faaed6a6ce0ce0b1e02048297e41d7c6dcd84e5d
云闪 <1773460341@qq.com>
2016-09-15 13:35:21 +0800

    重构init方法

--- src/org/yunshanmc/ycl/command/simple/SimpleCommand.java
+++ src/org/yunshanmc/ycl/command/simple/SimpleCommand.java
@@ -1,9 +1,14 @@
 package org.yunshanmc.ycl.command.simple;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import com.google.common.collect.Collections2;
+import org.bukkit.command.CommandSender;
+import org.yunshanmc.ycl.command.Command;
+import org.yunshanmc.ycl.command.simple.ArgConverter.ArgConverterFailException;
+import org.yunshanmc.ycl.message.Messager;
+import org.yunshanmc.ycl.message.NullMessager;
+import org.yunshanmc.ycl.utils.reflect.MethodFinder;
+
+import java.lang.annotation.*;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
@@ -12,16 +17,6 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
-import org.bukkit.command.CommandSender;
-import org.yunshanmc.ycl.command.Command;
-import org.yunshanmc.ycl.command.simple.ArgConverter.ArgConverterFailException;
-import org.yunshanmc.ycl.message.Messager;
-import org.yunshanmc.ycl.message.NullMessager;
-import org.yunshanmc.ycl.utils.reflect.ReflectionUtils;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
 
 /**
  * 简易命令类
@@ -42,7 +37,7 @@ public abstract class SimpleCommand implements Command {
     
     /**
      * @param name
-     *            命令名
+     *     命令名
      */
     public SimpleCommand(String name, ArgConverterManager argConverterManager) {
         init(this, argConverterManager);
@@ -79,7 +74,7 @@ public abstract class SimpleCommand implements Command {
     
     /**
      * 将命令设为有效
-     * 
+     *
      * @return 是否设置成功
      */
     protected boolean setVaild() {
@@ -88,7 +83,7 @@ public abstract class SimpleCommand implements Command {
     
     /**
      * 将命令设为无效
-     * 
+     *
      * @return 是否设置成功
      */
     protected boolean setInvaild() {
@@ -120,9 +115,9 @@ public abstract class SimpleCommand implements Command {
             return (boolean) this.commandHandler.invoke(sender, args);
         } catch (ArgConverterFailException e) {
             return this.onArgConvertFail(sender, e.getArg(), e.getConvertTo(), args);
-        } catch (WrongMethodTypeException | ClassCastException e) {
-            e.printStackTrace();
-        } catch (Throwable e) {
+        } catch (WrongMethodTypeException | ClassCastException e) {// 该项错误不应该出现
+            throw new Error("error", e);
+        } catch (Throwable e) {// 该项为命令处理方法抛出的异常
             e.printStackTrace();
         }
         return true;
@@ -130,39 +125,42 @@ public abstract class SimpleCommand implements Command {
     
     /**
      * 当命令发生者的类型不符合命令处理器的要求时会被调用
-     * 
+     *
      * @param sender
-     *            命令发生者
+     *     命令发生者
      * @param args
-     *            实际命令参数
-     * @return 命令语法是否正确(返回false将显示命令的默认帮助，不需要显示则返回true)
+     *     实际命令参数
+     *
+     * @return 命令语法是否正确(返回false将显示命令的默认帮助, 不需要显示则返回true)
      */
     protected boolean onSenderTypeDisallow(CommandSender sender, String... args) {
-        this.messager.info(sender, "command.restrictSenderType."+ this.senderType.getName().replace('.', '-'));
+        this.messager.info(sender, "command.restrictSenderType." + this.senderType.getName().replace('.', '-'));
         return true;
     }
     
     /**
-     * 当 <b>命令参数个数</b><code> &lt; </code><b>必填参数个数</b> 时会被调用
-     * 
+     * 当 <b>命令参数个数</b><code> < </code><b>必填参数个数</b> 时会被调用
+     *
      * @param sender
-     *            命令发生者
+     *     命令发生者
      * @param args
-     *            实际命令参数
-     * @return 命令语法是否正确(返回false将显示命令的默认帮助，不需要显示则返回true)
+     *     实际命令参数
+     *
+     * @return 命令语法是否正确(返回false将显示命令的默认帮助, 不需要显示则返回true)
      */
     protected boolean onTooLittleArgs(CommandSender sender, String... args) {
         return false;
     }
     
     /**
-     * 当 <b>命令参数个数</b><code> &gt; </code><b>必填参数个数+可选参数个数</b> 时会被调用
-     * 
+     * 当 <b>命令参数个数</b><code> > </code><b>必填参数个数+可选参数个数</b> 时会被调用
+     *
      * @param sender
-     *            命令发生者
+     *     命令发生者
      * @param args
-     *            实际命令参数
-     * @return 命令语法是否正确(返回false将显示命令的默认帮助，不需要显示则返回true)
+     *     实际命令参数
+     *
+     * @return 命令语法是否正确(返回false将显示命令的默认帮助, 不需要显示则返回true)
      */
     protected boolean onTooManyArgs(CommandSender sender, String... args) {
         return false;
@@ -172,64 +170,63 @@ public abstract class SimpleCommand implements Command {
      * 当参数转换器转换参数失败时会被调用
      * <p>
      * 当有参数转换失败后会立即调用此方法，不会继续尝试转换后续参数
-     * 
+     *
      * @param sender
-     *            命令发生者
+     *     命令发生者
      * @param arg
-     *            转换失败的参数
+     *     转换失败的参数
      * @param convertTo
-     *            转换失败的参数应该被转换成的类型
+     *     转换失败的参数应该被转换成的类型
      * @param args
-     *            实际命令参数
+     *     实际命令参数
+     *
      * @return 返回false将显示命令的默认帮助，不需要显示则返回true
      */
     protected boolean onArgConvertFail(CommandSender sender, String arg, Class<?> convertTo, String... args) {
         return false;
     }
     
+    private static final MethodFinder METHOD_FINDER = new MethodFinder().filterAnnotationPresent(CommandHandler.class)
+                                                                       .filterPublic()
+                                                                       .filterReturnType(boolean.class);
+    private static final Lookup LOOKUP = MethodHandles.lookup();
+    
     private static void init(SimpleCommand command, ArgConverterManager converterManager) {
         Class<? extends SimpleCommand> cls = command.getClass();
-        Method handler = ReflectionUtils.getFirstMethodByAnnotation(cls, CommandHandler.class);
+        
+        Method handler = METHOD_FINDER.findMethod(cls);
         if (handler == null) {
-            // 没有带命令处理器注解的方法
-            throw new SimpleCommandInitFailExecption(command, "none handler");
+            throw new SimpleCommandInitFailExecption(command, "none valid handler method in class" + cls.getName());
         }
-        if (handler.getReturnType() != boolean.class) {
-            // 返回值不是boolean类型
-            throw new SimpleCommandInitFailExecption(command, "return type not boolean");
+        
+        MethodHandle targetHandler;
+        try {
+            handler.setAccessible(true);
+            targetHandler = LOOKUP.unreflect(handler).bindTo(command);
+        } catch (IllegalAccessException e) {// 此异常不会出现
+            e.printStackTrace();
+            throw new SimpleCommandInitFailExecption(command,"error");
         }
+        
         CommandHandler handlerAnn = handler.getAnnotation(CommandHandler.class);
         Class<?>[] params = handler.getParameterTypes();
-        Lookup lookup = MethodHandles.lookup();
-        MethodHandle targetHandler = null;
-        try {
-            targetHandler = lookup.unreflect(handler).bindTo(command);
-        } catch (IllegalAccessException e1) {
-            // 方法不是public的
-            throw new SimpleCommandInitFailExecption(command, "handler not public");
-        }
-        if (params.length > 0) {
-            if (handlerAnn.needSender()) {
-                if (!CommandSender.class.isAssignableFrom(params[0])) {
-                    // 命令处理方法的第一个参数不是CommandSender类型或其子类型
-                    throw new SimpleCommandInitFailExecption(command,
-                            "the first arg is not receiver a CommandSender instance");
-                }
-                @SuppressWarnings("unchecked")
-                Class<? extends CommandSender> tmp = (Class<? extends CommandSender>) params[0];
-                command.senderType = tmp;
-                params = Arrays.copyOfRange(params, 1, params.length);
+        
+        if (handlerAnn.needSender()) {// 判断是否需要sender
+            if (params.length == 0 || !CommandSender.class.isAssignableFrom(params[0])) {
+                // 命令处理方法的第一个参数不是CommandSender类型或其子类型
+                throw new SimpleCommandInitFailExecption(command,
+                                                         "the first arg is not receiver a CommandSender instance");
             }
-            MethodHandle[] filters = null;
+            @SuppressWarnings("unchecked")
+            Class<? extends CommandSender> tmp = (Class<? extends CommandSender>) params[0];
+            command.senderType = tmp;
+            params = Arrays.copyOfRange(params, 1, params.length);
+        }
+        
+        if (params.length > 0) {
+            MethodHandle[] filters;
             List<ArgConverter<?>> converters = Arrays.asList(converterManager.getArgConverters(params));
-            Collection<MethodHandle> filterList = Collections2.transform(converters,
-                    new Function<ArgConverter<?>, MethodHandle>() {
-                        
-                        @Override
-                        public MethodHandle apply(ArgConverter<?> converter) {
-                            return converter.toMethodHandle();
-                        }
-                    });
+            Collection<MethodHandle> filterList = Collections2.transform(converters, ArgConverter::toMethodHandle);
             filters = filterList.toArray(new MethodHandle[filterList.size()]);
             if (handlerAnn.needSender()) {
                 targetHandler = MethodHandles.filterArguments(targetHandler, 1, filters);
@@ -240,14 +237,17 @@ public abstract class SimpleCommand implements Command {
             targetHandler = targetHandler.asSpreader(String[].class, filters.length);
         }
         command.maxArgsLength = params.length;
-        int optionIdx = ReflectionUtils.findFirstAnnotationIndexInParameters(handler, OptionalArgStart.class);
+        int optionIdx = -1;
+        Annotation[][] anns = handler.getParameterAnnotations();
+        for (int i = 0; i < anns.length; i++) {
+            for (Annotation ann : anns[i]) {
+                if (OptionalArgStart.class.isInstance(ann)) optionIdx = i;
+            }
+        }
         if (optionIdx != -1) {
             command.minArgsLength = optionIdx;
         } else {
             command.minArgsLength = command.maxArgsLength;
-        }
-        if (handlerAnn.needSender()) {// sender占了一个参数位置
-            command.minArgsLength -= 1;
         }
         command.commandHandler = targetHandler;
     }
@@ -263,7 +263,7 @@ public abstract class SimpleCommand implements Command {
          * 是否需要传参的时候在第一个参数传递命令发送者，默认为false
          * <p>
          * 该方法设为true的时候必须保证命令处理方法的第一个参数的类型是CommandSender或其子类
-         * 
+         *
          * @return 需要返回true，不需要返回false
          */
         boolean needSender() default false;
@@ -275,7 +275,7 @@ public abstract class SimpleCommand implements Command {
     @Target(ElementType.PARAMETER)
     @Retention(value = RetentionPolicy.RUNTIME)
     protected @interface OptionalArgStart {
-    
+        
     }
     
     /**
@@ -288,12 +288,14 @@ public abstract class SimpleCommand implements Command {
         
         private static final long serialVersionUID = 1L;
         
-        /** 未成功初始化的命令 */
+        /**
+         * 未成功初始化的命令
+         */
         private final SimpleCommand command;
         
         /**
          * @param command
-         *            未成功初始化的命令
+         *     未成功初始化的命令
          */
         public SimpleCommandInitFailExecption(SimpleCommand command, String reason) {
             super(command.getName() + ":" + reason);
@@ -302,7 +304,7 @@ public abstract class SimpleCommand implements Command {
         
         /**
          * 获取导致此异常抛出的未成功初始化的命令
-         * 
+         *
          * @return 未成功初始化的命令
          */
         protected SimpleCommand getCommand() {
